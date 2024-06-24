4aca70d8e3bf8bc902f5c676f189042368ff7d65
divinoPV <monteiro.hugo2001@icloud.com>
2024-06-24 10:26:26 +0000

    feature(model): methods signature

---
+++ src/kmeans/model.py
@@ -0,0 +1,12 @@
+class Kmeans:
+    def compression(self: 'Kmeans') -> None:
+        pass
+
+    def decrompression(self: 'Kmeans') -> None:
+        pass
+
+    def projection(self: 'Kmeans') -> None:
+        pass
+
+    def generation(self: 'Kmeans') -> None:
+        pass
---
+++ src/pca/model.py
@@ -0,0 +1,12 @@
+class PCA:
+    def compression(self: 'Kmeans') -> None:
+        pass
+
+    def decrompression(self: 'Kmeans') -> None:
+        pass
+
+    def projection(self: 'Kmeans') -> None:
+        pass
+
+    def generation(self: 'Kmeans') -> None:
+        pass
