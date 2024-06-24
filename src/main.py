fabc6c4c9c76caf06606ff9732a376273c72885e
David Degaugue <ddegaugue@myges.fr>
2024-06-24 14:47:54 +0200

    ajout main.py

--- src/main.py
+++ src/main.py
@@ -1,29 +1,27 @@
-import numpy as np
-from src.kmeans.model import kmeans
-
-#generate two clouds of points in 2D
-
-np.random.seed(6)
-n_samples = 1000
-n_clusters = 3
-
-X = np.random.randn(n_samples, 2)
-X[:300] += 5
-X[300:600] += 10
-X[600:] += 15
-
-#plot the data
+from kmeans.model import kmeans
 import matplotlib.pyplot as plt
+import numpy as np
 
-plt.scatter(X[:, 0], X[:, 1])
-plt.show()
+if __name__ == "__main__":
 
-print(X.shape)
+    np.random.seed(np.random.randint(100))
+    n_samples = 1000
+    n_clusters = 3
+
+    X = np.random.randn(n_samples, 2)
+    X[:300] += 5
+    X[300:600] += 10
+    X[600:] += 15
+
+    plt.scatter(X[:, 0], X[:, 1])
+    plt.show()
+
+    print(X.shape)
 
 
-#initialize the model
-kmeans = kmeans(n_clusters)
-kmeans.lloyd(X)
+    #initialize the model
+    kmeans = kmeans(n_clusters)
+    kmeans.lloyd(X)
 
-#plot the clusters
-kmeans.plot_clusters(X)
+    #plot the clusters
+    kmeans.plot_clusters(X)
